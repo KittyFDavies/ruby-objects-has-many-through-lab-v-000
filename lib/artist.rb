ff14e8bc7b88c3d14af6ac3387d04ad904d23692
Kitty Davies <kittyfdavies@gmail.com>
2018-05-13 22:36:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,17 +12,10 @@ class Artist
     @name = name
     @@all << self
   end
-  #def initialize(name)
-  #  @name = name
-  #  @@all << self
-  #end
 
   def self.all
     @@all
   end
-  #def self.all
-  #  @@all
-  #end
 
   def new_song(song, genre)
     Song.new(song, genre, self)
