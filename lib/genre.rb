49cf49596e101eb3d4f56eacc6d6fcdf0f8b61fe
Kitty Davies <kittyfdavies@gmail.com>
2018-05-13 22:34:59 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -38,26 +38,4 @@ class Genre
   end
 
 
-  
-
-  
-
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
-  end
-
-  def appointments
-    Appointment.all.select do |appt|
-      appt.doctor == self
-    end
-  end
-
-  def patients
-    appointments.map do |appt|
-      appt.patient
-    end
-  end
-
-
 end
