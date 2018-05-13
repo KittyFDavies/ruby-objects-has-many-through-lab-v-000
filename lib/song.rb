6bbeb55c7d4f218505f55df5ec44ac5b46c7502b
Kitty Davies <kittyfdavies@gmail.com>
2018-05-13 22:31:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ require 'pry'
 class Song
 
   attr_accessor :genre, :song, :artist
+  #attr_accessor :patient, :doctor, :date
 
   @@all = []
+  #@@all = []
 
   def initialize(song, genre, artist)
     @song = song
@@ -12,13 +14,23 @@ class Song
     @artist = artist
     @@all << self
   end
+  #def initialize(patient, doctor, date)
+  #  @patient = patient
+  #  @doctor = doctor
+  #  @date = date
+  #  @@all << self
+  #end
 
   def self.all
     @@all
   end
+  #def self.all
+  #  @@all
+  #end
 
   def genre
 
   end
+
 
 end
