2b8990700737a2ffa69988984f5b389c23a395e9
Kitty Davies <kittyfdavies@gmail.com>
2018-05-13 22:30:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,30 +3,52 @@ require 'pry'
 class Artist
 
   attr_accessor :name, :genre, :song
+  #attr_accessor :doctor, :date, :name
 
   @@all = []
+  #@@all = []
 
   def initialize(name)
     @name = name
     @@all << self
   end
+  #def initialize(name)
+  #  @name = name
+  #  @@all << self
+  #end
 
   def self.all
     @@all
   end
+  #def self.all
+  #  @@all
+  #end
+
+  def new_song(song, genre)
+    Song.new(song, genre, self)
+  end
+  #def new_appointment(doctor, date)
+  #  Appointment.new(self, doctor, date)
+  #end
 
   def songs
     Song.all.select do |song|
       song.artist == self
     end
   end
+  #def appointments
+  #  Appointment.all.select do |appt|
+  #    appt.patient == self
+  #  end
+  #end
 
   def genres
-    songs.select {|song| song.genre}
+    songs.map {|song| song.genre}
   end
-
-  def new_song(song, genre)
-    Song.new(song, genre, self)
-  end
+  #def doctors
+  #  appointments.map do |appt|
+  #    appt.doctor
+  #  end
+  #end
 
 end
